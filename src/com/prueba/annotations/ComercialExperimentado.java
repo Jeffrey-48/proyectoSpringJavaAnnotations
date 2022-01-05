af207a50108a443162e91bf677258c30c12c11ed
Jeffrey portatil <javargasg_1@uqvirtual.edu.co>
2022-01-05 13:08:42 -0500

    @PostConstruct y @PreDestroy con altenativas InitializingBean, DisposableBean

--- src/com/prueba/annotations/ComercialExperimentado.java
+++ src/com/prueba/annotations/ComercialExperimentado.java
@@ -1,5 +1,7 @@
 package com.prueba.annotations;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
@@ -7,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component("ComercialExperimentado")
 //definir el patron
-@Scope("prototype")
-public class ComercialExperimentado implements Empleado {
+//@Scope("prototype")
+public class ComercialExperimentado implements Empleado,  InitializingBean, DisposableBean{
 
 	@Autowired
 	@Qualifier("informeFinancieroTrim4") //bean id que debe usar
@@ -37,5 +39,17 @@ public class ComercialExperimentado implements Empleado {
 //		this.nuevoInforme = nuevoInforme;
 //	}
 	
+	//ejecución de código después de la creación del bean
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("codigo ejecutado tras creacion de bean");
+	}
+	
+	//ejecucion de código después de apagado el contenedor
+	public void destroy() {
+		System.out.println("ejecutando antes de la destruccion");
+	}
+
 	
 }
